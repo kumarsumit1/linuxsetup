a660973f6424b8e0f5230a89e0bfa641db1077ca
kumarsumit1 <ursumit@gmail.com>
2020-07-13 16:02:42 +0530

    Update cloudBackup.sh

--- cloudBackup.sh
+++ cloudBackup.sh
@@ -30,4 +30,11 @@
 /usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --bwlimit 200k --stats 1s "/home/mukesh/Downloads/cloud" "gdrive:backup"
 
 # notify-send uses icon from the /usr/share/icons/gnome/32x32 (& also /usr/share/notify-osd/icons/...)
-notify-send -u critical -i "network-wireless.png" 'Cloud Sync Update' 'Success'
+ if [ $? -ne 0 ]
+  then
+        notify-send -u critical -i "network-wireless.png" 'Cloud Sync Update' 'Failure'
+        exit 1
+ else
+        notify-send -u critical -i "network-wireless.png" 'Cloud Sync Update' 'Success'
+ fi
+
